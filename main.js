82fe8ce4f45d1475c62d60db7e9ebe9a17bbc593
jpablomaure <jpablomaure@gmail.com>
2022-03-18 11:59:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let molestar = prompt("Introduzca el nombre de la persona que quiere molestar");
+
+molestar = molestar.toUpperCase();
+alert(molestar);
+
+comenzar(molestar);
+
+
+function comenzar(molestaAQuien){
+while (molestaAQuien == "VANESA"){
+    alert("ES HORA DE SUPERAR LA COSAS, YA NO PUEDES MOLESTAR A " +molestaAQuien);
+    molestaAQuien.toUpperCase() = prompt("Introduzca el nombre de la persona que quiere molestar");
+}
+};
+
+alert("BIEN, EL PRIMER PASO ESTA DADO, AUNQUE NO DEBERIAS MOLESTAR A NADIE MAS, INCLUSO A " +molestar);
+
+
+
